5e0ff7a44666359ecb2201c3995de818f6b184ea
Jake Deerin <2016deerinjo@gmail.com>
2020-04-28 17:12:22 -0400

    Removed tutorial-specific HTML code from page, minor changes to comments

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Show we can define variables, make comments, print to console, define functions
 // ALSO: Note that by default, it will just start running this script from the top-down as soon as it's loaded
+
 const PI = 3.2;  // There is nothing wrong with this in Indiana in 1897
 let circleRadius = 2*PI;
 
@@ -9,7 +10,7 @@ function circle_area(radius, isFromIndiana) {
     const THE_ONCE_AND_FUTURE_PI = 3.1415926;
     if (isFromIndiana) {
         return radius*radius * PI;
-    } else {    // Remove else after doing this to remind people it isn't necessary, because of the return
+    } else {    // Else is optional here, but it's more explicit
         return radius*radius * THE_ONCE_AND_FUTURE_PI;
     }
 }
@@ -21,7 +22,7 @@ for (let radius = 1; radius < 5; radius++) {
 }
 
 //==============================================================================
-// ACTUAL STUFF (bring up why defer statement was needed when we first refer to an HTML element)
+// ACTUAL STUFF (bring up why defer statement was needed in the HTML when we first refer to an HTML element, so that all the elements load before we try to get them)
 
 let temperature = 85;
 
@@ -70,13 +71,13 @@ const JIGGLE_ELEMENTS = [
         "element": DAVID_HEAD,
         "original_left": -1,
         "original_top": -1,
-        "jiggle_amount": 3
+        "jiggle_amount": 20
     },
     {
         "element": OPPONENT_HEAD,
         "original_left": -1,
         "original_top": -1,
-        "jiggle_amount": 3
+        "jiggle_amount": 20
     }
 ]
 
@@ -89,7 +90,8 @@ for (let i=0; i < JIGGLE_ELEMENTS.length; i++) {
 console.log(JIGGLE_ELEMENTS);
 
 // Don't go into details on async function, just explain that it means other stuff can run/it won't block BUT we can't depend on anything outside it being run in a certain order after it's called
-async function jiggle_elements() {
+// Actually, I've chosen not to make this async for clarity's sake
+function jiggle_elements() {
     JIGGLE_ELEMENTS.forEach((jiggleEl) => {
         jiggleEl.element.style.left = `${jiggleEl.original_left
             + Math.random()*jiggleEl.jiggle_amount}px`;
@@ -104,4 +106,4 @@ jiggle_elements();
 
 // finally, introduce arrow functions as a shorthand
 const FIGHT_BUTTON = document.getElementById("david-dojo-fight");
-FIGHT_BUTTON.addEventListener("click", () => {JIGGLE_ELEMENTS[0].jiggle_amount = 20});
+FIGHT_BUTTON.addEventListener("click", () => {JIGGLE_ELEMENTS[0].jiggle_amount = 200});
